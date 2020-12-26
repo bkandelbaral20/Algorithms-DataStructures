d7db324792191c060ace578631644500562bd444
Binjita <binjita.baral@gmail.com>
2020-12-25 21:27:53 -0600

    return the sum of the array elements as an integer

---
+++ src/Algorithms/ArraySum.java
@@ -0,0 +1,66 @@
+package Algorithms;
+
+/*
+Given an array of integers, find the sum of its elements.
+
+For example, if the array ar=[1,2,3] = 1+2+3 = 6,so return 6.
+
+Function Description
+
+Complete the simpleArraySum function in the editor below. It must return the sum of the array elements as an integer.
+
+simpleArraySum has the following parameter(s):
+ar: an array of integers
+
+Input Format
+The first line contains an integer, n , denoting the size of the array.
+The second line contains n space-separated integers representing the array's elements.
+
+Output Format
+Print the sum of the array's elements as a single integer.
+
+ */
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class ArraySum {
+
+    static int simpleArraySum(int[] ar) {
+
+        int sum = 0;
+        for (int i = 0; i< ar.length; i++) {
+            sum += ar[i];
+        }
+        return sum;
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+//        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+//
+//        int arCount = Integer.parseInt(scanner.nextLine().trim());
+//
+//        int[] ar = new int[arCount];
+//
+//        String[] arItems = scanner.nextLine().split(" ");
+//
+//        for (int arItr = 0; arItr < arCount; arItr++) {
+//            int arItem = Integer.parseInt(arItems[arItr].trim());
+//            ar[arItr] = arItem;
+//        }
+//
+//        int result = simpleArraySum(ar);
+//
+//        bufferedWriter.write(String.valueOf(result));
+//        bufferedWriter.newLine();
+//
+//        bufferedWriter.close();
+
+        int [] ar1 = {1,2,3};
+        System.out.println(ArraySum.simpleArraySum(ar1));
+    }
+}
