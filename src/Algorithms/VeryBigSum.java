da8b088c3a30709f1f92ffb18e12984b6550022d
Binjita <binjita.baral@gmail.com>
2020-12-26 20:52:15 -0600

    the sum of all array elements

---
+++ src/Algorithms/VeryBigSum.java
@@ -0,0 +1,52 @@
+package Algorithms;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class VeryBigSum {
+
+    // Complete the aVeryBigSum function below.
+    static long aVeryBigSum(long[] ar) {
+
+        long sum = 0;
+        for(int i =0; i<ar.length; i++){
+            sum += ar[i];
+        }
+        return sum;
+
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+//        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+//
+//        int arCount = scanner.nextInt();
+//        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+//
+//        long[] ar = new long[arCount];
+//
+//        String[] arItems = scanner.nextLine().split(" ");
+//        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+//
+//        for (int i = 0; i < arCount; i++) {
+//            long arItem = Long.parseLong(arItems[i]);
+//            ar[i] = arItem;
+//        }
+//
+//        long result = aVeryBigSum(ar);
+//
+//        bufferedWriter.write(String.valueOf(result));
+//        bufferedWriter.newLine();
+//
+//        bufferedWriter.close();
+
+        scanner.close();
+        long [] ar1 = {1000000001, 1000000002, 1000000003, 1000000004, 1000000005};
+        System.out.println("The total sum of an array is " + VeryBigSum.aVeryBigSum(ar1));
+
+
+    }
+}
